0cfba79b1a5210e788f9a7be524ca5bab88d04f8
blomq <blomqvistkevin@hotmail.com>
2025-07-12 18:01:00 +0200

    Add `database` module with user, room, and game management functionality

---
+++ include/database.hpp
@@ -0,0 +1,52 @@
+﻿#ifndef DATABASE_HPP
+#define DATABASE_HPP
+
+#include <memory>
+#include <unordered_map>
+
+#include <boost/asio/ip/address_v4.hpp>
+
+namespace worms_server
+{
+	class user;
+	class room;
+	class game;
+
+	class database : std::enable_shared_from_this<database>
+	{
+	public:
+		static std::weak_ptr<database> get_instance();
+		static uint32_t get_next_id();
+
+		[[nodiscard]] std::weak_ptr<user> get_user(uint32_t id) const;
+		[[nodiscard]] std::weak_ptr<room> get_room(uint32_t id) const;
+		[[nodiscard]] std::weak_ptr<game> get_game(uint32_t id) const;
+
+		[[nodiscard]] std::vector<std::weak_ptr<user>> get_users() const;
+		[[nodiscard]] std::vector<std::weak_ptr<room>> get_rooms() const;
+		[[nodiscard]] std::vector<std::weak_ptr<game>> get_games() const;
+
+		std::vector<std::weak_ptr<user>> get_users_in_room(uint32_t room_id) const;
+
+		void add_user(std::shared_ptr<user>&& user);
+		void remove_user(uint32_t id);
+
+		void add_room(std::shared_ptr<room>&& room);
+		void remove_room(uint32_t id);
+
+		void add_game(std::shared_ptr<game>&& game);
+		void remove_game(uint32_t id);
+
+	private:
+		mutable std::shared_mutex _users_mutex;
+		mutable std::shared_mutex _rooms_mutex;
+		mutable std::shared_mutex _games_mutex;
+		std::atomic_uint32_t _next_id = 0x1000;
+
+		std::unordered_map<uint32_t, std::shared_ptr<user>> _users;
+		std::unordered_map<uint32_t, std::shared_ptr<room>> _rooms;
+		std::unordered_map<uint32_t, std::shared_ptr<game>> _games;
+	};
+}
+
+#endif //DATABASE_HPP
---
+++ src/database.cpp
@@ -0,0 +1,136 @@
+﻿//
+// Created by blomq on 2025-07-10.
+//
+
+#include "database.hpp"
+
+#include <ranges>
+
+#include "game.hpp"
+#include "room.hpp"
+#include "user.hpp"
+
+std::weak_ptr<worms_server::database> worms_server::database::get_instance()
+{
+	static auto inst = std::make_shared<database>();
+	return inst;
+}
+
+uint32_t worms_server::database::get_next_id()
+{
+	const auto instance = get_instance().lock();
+	if (instance == nullptr) return 0;
+
+	const uint32_t next_id = instance->_next_id.fetch_add(1, std::memory_order::relaxed);
+
+	return next_id;
+}
+
+std::weak_ptr<worms_server::user> worms_server::database::get_user(const uint32_t id) const
+{
+	std::shared_lock lock(_users_mutex);
+	const auto it = _users.find(id);
+	return it != _users.end() ? it->second : nullptr;
+}
+
+std::weak_ptr<worms_server::room> worms_server::database::get_room(const uint32_t id) const
+{
+	std::shared_lock lock(_rooms_mutex);
+	const auto it = _rooms.find(id);
+	return it != _rooms.end() ? it->second : nullptr;
+}
+
+std::weak_ptr<worms_server::game> worms_server::database::get_game(const uint32_t id) const
+{
+	std::shared_lock lock(_games_mutex);
+	const auto it = _games.find(id);
+	return it != _games.end() ? it->second : nullptr;
+}
+
+std::vector<std::weak_ptr<worms_server::user>> worms_server::database::get_users() const
+{
+	std::shared_lock lock(_users_mutex);
+	std::vector<std::weak_ptr<user>> users;
+
+	users.reserve(_users.size());
+	std::ranges::copy(_users | std::views::values, std::back_inserter(users));
+
+	return users;
+}
+
+std::vector<std::weak_ptr<worms_server::room>> worms_server::database::get_rooms() const
+{
+	std::shared_lock lock(_rooms_mutex);
+	std::vector<std::weak_ptr<room>> rooms;
+
+	rooms.reserve(_rooms.size());
+	std::ranges::copy(_rooms | std::views::values, std::back_inserter(rooms));
+	return rooms;
+}
+
+std::vector<std::weak_ptr<worms_server::game>> worms_server::database::get_games() const
+{
+	std::shared_lock lock(_games_mutex);
+	std::vector<std::weak_ptr<game>> games;
+
+	games.reserve(_games.size());
+	std::ranges::copy(_games | std::views::values, std::back_inserter(games));
+	return games;
+}
+
+std::vector<std::weak_ptr<worms_server::user>> worms_server::database::get_users_in_room(const uint32_t room_id) const
+{
+	std::shared_lock users_lock(_users_mutex);
+	std::shared_lock rooms_lock(_rooms_mutex);
+	std::vector<std::weak_ptr<user>> users;
+
+	users.reserve(_rooms.size());
+	for (const auto& user : _users | std::views::values)
+	{
+		if (user->get_room_id() == room_id)
+		{
+			users.push_back(user);
+		}
+	}
+
+	return users;
+}
+
+void worms_server::database::add_user(std::shared_ptr<user>&& user)
+{
+	std::unique_lock lock(_users_mutex);
+	const uint32_t id = user->get_id();
+	_users[id] = std::move(user);
+}
+
+void worms_server::database::remove_user(const uint32_t id)
+{
+	std::unique_lock lock(_users_mutex);
+	_users.erase(id);
+}
+
+void worms_server::database::add_room(std::shared_ptr<room>&& room)
+{
+	std::unique_lock lock(_rooms_mutex);
+	const uint32_t id = room->get_id();
+	_rooms[id] = std::move(room);
+}
+
+void worms_server::database::remove_room(const uint32_t id)
+{
+	std::unique_lock lock(_rooms_mutex);
+	_rooms.erase(id);
+}
+
+void worms_server::database::add_game(std::shared_ptr<game>&& game)
+{
+	std::unique_lock lock(_games_mutex);
+	const uint32_t id = game->get_id();
+	_games[id] = std::move(game);
+}
+
+void worms_server::database::remove_game(const uint32_t id)
+{
+	std::unique_lock lock(_games_mutex);
+	_games.erase(id);
+}
