a38d8ff2e29415887ab2f63bf2bc3e3cbbc6266c
blomq <blomqvistkevin@hotmail.com>
2025-07-15 22:29:12 +0200

    Fix conditional logic: correct `target_user` room ID check in `packet_handler.cpp`.

--- src/packet_handler.cpp
+++ src/packet_handler.cpp
@@ -122,7 +122,7 @@ namespace worms_server
             std::format("PRV:[ {} ]  ", client_user->get_name())))
         {
             const auto& target_user = database->get_user(target_id);
-            if (target_user == nullptr || !target_user->get_room_id() != client_room_id)
+            if (target_user == nullptr || target_user->get_room_id() != client_room_id)
             {
                 client_user->send_packet(
                     worms_packet::freeze(packet_code::chat_room_reply,
